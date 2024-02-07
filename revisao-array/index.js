8007ea5725fb87f33334cdc83c727ac86ef12cd5
Alex <alexgaban@hotmail.com>
2024-02-06 23:13:28 -0300

    new: aula 06-02

---
+++ revisao-array/index.js
@@ -0,0 +1,25 @@
+variavel simples - String, Number, Boolean,
+variavel composta - Array, objeto
+
+
+const nomes = ['Daniel', 'Diehl', 'Bruno']
+const cursos = ['JS', 'Backend', 'React']
+const hobbies = ['Ler livros', 'Caminhar', 'Praticar esporte']
+
+array = []
+objeto = {}
+
+const array = [];
+const usuarios = [{
+    nome: 'Bruno',
+    curso: 'Backend',
+    hobby: 'Ler livros'
+},{
+    nome: 'Diehl',
+    curso: 'JS',
+    hobby: 'Caminhar'
+},{
+    nome: 'Bruno',
+    curso: 'React',
+    hobby: 'Praticar esporte'
+}]
